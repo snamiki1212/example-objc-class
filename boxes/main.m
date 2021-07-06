63b6919053eaaf49df24317699bd6955267c20e7
snamiki1212 <snamiki1212@gmail.com>
2021-07-06 10:50:54 -0700

    Add skelton

---
+++ boxes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  boxes
+//
+//  Created by shunnamiki on 2021/07/06.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
